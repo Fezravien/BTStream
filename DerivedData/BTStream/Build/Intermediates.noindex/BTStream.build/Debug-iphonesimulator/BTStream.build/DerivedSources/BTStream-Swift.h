c2f5bed612745711a1d7d9831bfb4380fa459527
SeoHyeokGyu <shk64099@gmail.com>
2020-11-02 14:52:14 +0900

    home UI 네비게이션 컨트롤러 추가 스택뷰, 라벨 추가

--- DerivedData/BTStream/Build/Intermediates.noindex/BTStream.build/Debug-iphonesimulator/BTStream.build/DerivedSources/BTStream-Swift.h
+++ DerivedData/BTStream/Build/Intermediates.noindex/BTStream.build/Debug-iphonesimulator/BTStream.build/DerivedSources/BTStream-Swift.h
@@ -188,9 +188,6 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 #if __has_warning("-Watimport-in-framework-header")
 #pragma clang diagnostic ignored "-Watimport-in-framework-header"
 #endif
-@import AuthenticationServices;
-@import Foundation;
-@import GoogleSignIn;
 @import UIKit;
 #endif
 
@@ -209,17 +206,13 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 # pragma pop_macro("any")
 #endif
 
-@class GIDSignIn;
-@class GIDGoogleUser;
 @class UIApplication;
 @class UISceneSession;
 @class UISceneConnectionOptions;
 @class UISceneConfiguration;
 
 SWIFT_CLASS("_TtC8BTStream11AppDelegate")
-@interface AppDelegate : UIResponder <GIDSignInDelegate, UIApplicationDelegate>
-- (void)signIn:(GIDSignIn * _Null_unspecified)signIn didSignInForUser:(GIDGoogleUser * _Null_unspecified)user withError:(NSError * _Null_unspecified)error;
-- (BOOL)application:(UIApplication * _Nonnull)application openURL:(NSURL * _Nonnull)url sourceApplication:(NSString * _Nullable)sourceApplication annotation:(id _Nonnull)annotation SWIFT_WARN_UNUSED_RESULT;
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
 - (BOOL)application:(UIApplication * _Nonnull)application didFinishLaunchingWithOptions:(NSDictionary<UIApplicationLaunchOptionsKey, id> * _Nullable)launchOptions SWIFT_WARN_UNUSED_RESULT;
 - (UISceneConfiguration * _Nonnull)application:(UIApplication * _Nonnull)application configurationForConnectingSceneSession:(UISceneSession * _Nonnull)connectingSceneSession options:(UISceneConnectionOptions * _Nonnull)options SWIFT_WARN_UNUSED_RESULT;
 - (void)application:(UIApplication * _Nonnull)application didDiscardSceneSessions:(NSSet<UISceneSession *> * _Nonnull)sceneSessions;
@@ -245,26 +238,15 @@ SWIFT_CLASS("_TtC8BTStream18FeedViewController")
 
 SWIFT_CLASS("_TtC8BTStream18HomeViewController")
 @interface HomeViewController : UIViewController
+- (void)viewDidLoad;
 - (nonnull instancetype)initWithNibName:(NSString * _Nullable)nibNameOrNil bundle:(NSBundle * _Nullable)nibBundleOrNil OBJC_DESIGNATED_INITIALIZER;
 - (nullable instancetype)initWithCoder:(NSCoder * _Nonnull)coder OBJC_DESIGNATED_INITIALIZER;
 @end
 
-@class UIStackView;
-@class GIDSignInButton;
-@class ASAuthorizationController;
-@class ASAuthorization;
 
 SWIFT_CLASS("_TtC8BTStream19LoginViewController")
-@interface LoginViewController : UIViewController <ASAuthorizationControllerDelegate, ASAuthorizationControllerPresentationContextProviding, GIDSignInDelegate>
-@property (nonatomic, weak) IBOutlet UIStackView * _Null_unspecified appleSignInButton;
-@property (nonatomic, weak) IBOutlet GIDSignInButton * _Null_unspecified googleSignInButton;
-- (void)signIn:(GIDSignIn * _Null_unspecified)signIn didSignInForUser:(GIDGoogleUser * _Null_unspecified)user withError:(NSError * _Null_unspecified)error;
-- (void)signIn:(GIDSignIn * _Null_unspecified)signIn didDisconnectWithUser:(GIDGoogleUser * _Null_unspecified)user withError:(NSError * _Null_unspecified)error;
-- (void)appleSignInButtonPress;
-- (ASPresentationAnchor _Nonnull)presentationAnchorForAuthorizationController:(ASAuthorizationController * _Nonnull)controller SWIFT_WARN_UNUSED_RESULT;
-- (void)authorizationController:(ASAuthorizationController * _Nonnull)controller didCompleteWithAuthorization:(ASAuthorization * _Nonnull)authorization;
-- (void)authorizationController:(ASAuthorizationController * _Nonnull)controller didCompleteWithError:(NSError * _Nonnull)error;
-- (void)viewDidLoad;
+@interface LoginViewController : UIViewController
+- (IBAction)loginBtn:(id _Nonnull)sender;
 - (nonnull instancetype)initWithNibName:(NSString * _Nullable)nibNameOrNil bundle:(NSBundle * _Nullable)nibBundleOrNil OBJC_DESIGNATED_INITIALIZER;
 - (nullable instancetype)initWithCoder:(NSCoder * _Nonnull)coder OBJC_DESIGNATED_INITIALIZER;
 @end
@@ -296,14 +278,6 @@ SWIFT_CLASS("_TtC8BTStream13SceneDelegate")
 
 SWIFT_CLASS("_TtC8BTStream17SetViewController")
 @interface SetViewController : UIViewController
-- (nonnull instancetype)initWithNibName:(NSString * _Nullable)nibNameOrNil bundle:(NSBundle * _Nullable)nibBundleOrNil OBJC_DESIGNATED_INITIALIZER;
-- (nullable instancetype)initWithCoder:(NSCoder * _Nonnull)coder OBJC_DESIGNATED_INITIALIZER;
-@end
-
-
-SWIFT_CLASS("_TtC8BTStream14ViewController")
-@interface ViewController : UIViewController
-- (void)viewDidLoad;
 - (nonnull instancetype)initWithNibName:(NSString * _Nullable)nibNameOrNil bundle:(NSBundle * _Nullable)nibBundleOrNil OBJC_DESIGNATED_INITIALIZER;
 - (nullable instancetype)initWithCoder:(NSCoder * _Nonnull)coder OBJC_DESIGNATED_INITIALIZER;
 @end
