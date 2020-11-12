a2e8bd1a11b2f31a51fe3c5dbcb9a130a317db3a
SeoHyeokGyu <shk64099@gmail.com>
2020-11-12 18:02:40 +0900

    상단바 네비게이션컨트롤러추가

--- BTStream/CalendarViewController.swift
+++ BTStream/CalendarViewController.swift
@@ -17,11 +17,23 @@ import FSCalendar
 
 class CalendarViewController: UIViewController,FSCalendarDataSource, FSCalendarDelegate, FSCalendarDelegateAppearance{
     let dateFormatter = DateFormatter()
+    
     @IBOutlet var calendar: FSCalendar!
     @IBOutlet weak var dateIdenfier: UITextView!
+//    @IBOutlet weak var NtitleView: UINavigationBar!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let LtitleView = UILabel(frame: CGRect(x: 0, y: 0, width: self.view.frame.width , height: (self.navigationController?.navigationBar.frame.height)!))
+        LtitleView.textAlignment = .center
+        LtitleView.textColor = .systemBlue
+        LtitleView.font = .systemFont(ofSize: 28, weight: .heavy)
+        LtitleView.text = "BTStream"
+//        NtitleView.topItem?.titleView = LtitleView
+        self.navigationItem.titleView = LtitleView
+        
         
         dateFormatter.dateFormat = "yyyy-MM-dd"
         calendar.allowsMultipleSelection = true
@@ -37,19 +49,19 @@ class CalendarViewController: UIViewController,FSCalendarDataSource, FSCalendarD
         dateIdenfier.text=(dateFormatter.string(from: date) + " 선택됨")
     }
     func calendar(_ calendar: FSCalendar, subtitleFor date: Date) -> String? {
-            
-            switch dateFormatter.string(from: date) {
-            case dateFormatter.string(from: Date()):
-                return "오늘"
-            case "2020-11-22":
-                return "출근"
-            case "2020-11-23":
-                return "지각"
-            case "2020-11-24":
-                return "결근"
-            default:
-                return nil
-            }
+        
+        switch dateFormatter.string(from: date) {
+        case dateFormatter.string(from: Date()):
+            return "오늘"
+        case "2020-11-22":
+            return "출근"
+        case "2020-11-23":
+            return "지각"
+        case "2020-11-24":
+            return "결근"
+        default:
+            return nil
         }
+    }
 }
 
