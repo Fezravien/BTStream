6e681c5c8659cb53d1683dec9a75cded67b762ac
pazrvien <yjaewoong@naver.com>
2020-11-17 15:50:35 +0900

    상단 바 버튼 이벤트 -> 홈 기사 비동기 처리

--- BTStream/FeedViewController.swift
+++ BTStream/FeedViewController.swift
@@ -9,4 +9,13 @@ import UIKit
 
 class FeedViewController: UIViewController {
     
+    @IBAction func homebtn(_ sender: Any) {
+        guard let hb = storyboard?.instantiateViewController(identifier: "tabBar") else {
+            return
+        }
+
+        hb.modalPresentationStyle = .fullScreen
+
+        present(hb, animated: true, completion: nil)
+    }
 }
