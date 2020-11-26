c45e984b80703a4de456368dcbac9d4124f786f0
SeoHyeokGyu <shk64099@gmail.com>
2020-11-27 01:46:25 +0900

    캘린더 날짜 선택하면 밑에 라벨에 그날 일정 표시

--- BTStream/CalendarViewController.swift
+++ BTStream/CalendarViewController.swift
@@ -19,7 +19,7 @@ class CalendarViewController: UIViewController,FSCalendarDataSource, FSCalendarD
     let dateFormatter = DateFormatter()
     @IBOutlet var calendar: FSCalendar!
     @IBOutlet weak var dateIdenfier: UITextView!
-    
+    var dic1  = ["2020-11-22":"유튜브 BTS VACATION LOG ","2020-11-23":"V앱 RUN BTS", "2020-11-24":"콘서트 BTS 월드 투어"]
     @IBAction func homeBtn(_ sender: Any) {
         guard let tb = storyboard?.instantiateViewController(identifier: "tabBar") else {
             return
@@ -44,7 +44,16 @@ class CalendarViewController: UIViewController,FSCalendarDataSource, FSCalendarD
     }
     func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
         print(dateFormatter.string(from: date) + " 선택됨")
-        dateIdenfier.text=(dateFormatter.string(from: date) + " 선택됨")
+        var str = ""
+        for (date1,vlue) in dic1{
+            if(dateFormatter.string(from: date) == (date1)){
+                str = vlue
+            }
+            dateIdenfier.text = "일정 : " + str
+
+            
+        }
+        
     }
     func calendar(_ calendar: FSCalendar, subtitleFor date: Date) -> String? {
             
